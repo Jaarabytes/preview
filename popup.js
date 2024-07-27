b5e266f4600de784f8cee907fb5ff2f2b18d52f3
jaarabytes <xavierandole78@gmail.com>
2024-07-27 22:23:03 +0300

    boilerplate

---
+++ popup.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.getElementById('myButton');
+  const slider = document.getElementById('volumeSlider');
+
+  button.addEventListener('click', function() {
+  });
+
+  slider.addEventListener('input', function() {
+    changeVolume(slider.value);
+  });
+
+  function previewProfilePicture(value) {
+  }
+});
